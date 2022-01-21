e2c7852b2c36a23fb85346b5ddb4cbf3df7c219c
Ayush Karn <ayush2599@gmail.com>
2022-01-21 18:14:06 +0545

    Search bar mobile responsive

--- components/Header.js
+++ components/Header.js
@@ -58,13 +58,13 @@ function Header() {
           />
         </div>
         {/* Middle - Search*/}
-        <div className="max-w-xs">
+        <div className="max-w-xs mx-5">
           <div className="relative mt-1 p-3 rounded-md">
             <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
               <SearchIcon className="h-5 w-5 text-gray-500" />
             </div>{" "}
             <input
-              className="bg-gray-100 block w-3/5 md:w-full pl-10 sm:text-sm border-0 focus:ring-0 focus:border-black rounded-md"
+              className="bg-gray-100 block w-full md:w-full pl-10 sm:text-sm border-0 focus:ring-0 focus:border-black rounded-md"
               type="text"
               placeholder="Search"
             ></input>{" "}
@@ -103,7 +103,7 @@ function Header() {
             </>
           ) : (
             <buton onClick={signIn} className=" cursor-pointer">
-              Sign In
+              Login
             </buton>
           )}
         </div>
